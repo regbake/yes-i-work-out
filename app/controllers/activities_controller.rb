25a00eb301e0dd646f3e17a20a23115de9fa9817
regbake <regbake@gmail.com>
2017-11-11 21:24:43 -0800

    set up user model; some controllers

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,19 @@
+class ActivitiesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'activities/index'
+
+  get 'activities/new'
+
+  get 'activities/edit'
+
+  get 'activities/show'
+
+  get 'activities/update'
+
+  get 'activities/destroy'
+
+  root 'main#index'
+
+  get 'about' => 'main#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
